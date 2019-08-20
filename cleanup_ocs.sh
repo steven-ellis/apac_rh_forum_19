832360feb917a8ea38b210ec9f779d0b98c08642
Steven Ellis <steven.ellis@gmail.com>
2019-08-20 15:46:09 +1200

    Add script to clean up existing OCS deployment

---
+++ cleanup_ocs.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#
+# Script to clean up an existing Rook-Ceph environment
+#
+
+source ocp.env
+
+OCP_NAMESPACE=rook-ceph
+
+oc login -u ${OCP_USER} -p ${OCP_PASS} ${OCP_ENDPOINT} --insecure-skip-tls-verify=false
+
+# Delete the project
+oc delete project ${OCP_NAMESPACE}
+
+
+# Create the storage cluster
+delete_ceph_storage_cluster ()
+{
+    oc get machinesets -n openshift-machine-api
+
+    CLUSTERID=$(oc get machineset -n openshift-machine-api -o jsonpath='{.items[0].metadata.labels.machine\.openshift\.io/cluster-api-cluster}')
+    echo $CLUSTERID
+
+    mkdir -p storage_cluster
+
+    cp ./content/support/cluster-workerocs-*.yaml --target-directory=./storage_cluster/
+    
+    sed -i "s/cluster-28cf-t22gs/$CLUSTERID/g" ./storage_cluster/cluster-workerocs-*.yaml
+
+    oc delete -f ./storage_cluster/cluster-workerocs-us-east-2a.yaml
+    oc delete -f ./storage_cluster/cluster-workerocs-us-east-2b.yaml
+    oc delete -f ./storage_cluster/cluster-workerocs-us-east-2c.yaml
+
+    oc get machines -n openshift-machine-api
+    sleep 10s
+
+    # We now need to wait for them to all be created
+    oc get machinesets -n openshift-machine-api
+
+    # Confirm we've got the environment
+    oc get nodes -l node-role.kubernetes.io/worker
+
+    sleep 10s
+
+    # We shouldn't have any
+    echo "confirming we have no nodes tagged as storage-node"
+    oc get nodes --selector role=storage-node
+}
+
+delete_ceph_storage_cluster 
