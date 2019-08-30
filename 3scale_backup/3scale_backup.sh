4f4c866f6939fd8d8d1329a70fa959ce702bc9d7
Steven Ellis <steven.ellis@gmail.com>
2019-08-30 15:08:46 +1200

    Initial backup script for 3scale

---
+++ 3scale_backup/3scale_backup.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#
+# This is based on https://github.com/3scale/3scale-Operations/blob/master/docs/day-2-operations/Backups.adoc
+#
+
+BACKUP_DIR=${1:-backup}
+
+# Source our environment
+source ../ocp.env
+source ../functions
+
+# And login as the kubeadmin user
+
+oc login -u ${OCP_USER} -p ${OCP_PASS} ${OCP_ENDPOINT} --insecure-skip-tls-verify=false
+
+echo "Creating a new 3scale backup into directory ${BACKUP_DIR}"
+
+if [ ! -d ${BACKUP_DIR} ]; then
+    mkdir -p ${BACKUP_DIR}
+fi
+
+echo "Backup mySQL"
+oc rsh $(oc get pods -l 'deploymentConfig=system-mysql' -o json | jq -r '.items[0].metadata.name') bash -c 'export MYSQL_PWD=${MYSQL_ROOT_PASSWORD}; mysqldump --single-transaction -hsystem-mysql -uroot system' | gzip > ${BACKUP_DIR}/system-mysql-backup.gz
+
+
+echo "Backup System App"
+mkdir -p ${BACKUP_DIR}/system
+oc rsync $(oc get pods -l 'deploymentConfig=system-app' -o json | jq '.items[0].metadata.name' -r):/opt/system/public/system ${BACKUP_DIR}/system/
+
+echo "Backup zync database"
+oc rsh $(oc get pods -l 'deploymentConfig=zync-database' -o json | jq '.items[0].metadata.name' -r) bash -c 'pg_dumpall -c --if-exists' | gzip > ${BACKUP_DIR}/zync-database-backup.gz
+
+echo "Backup backend-redis"
+
+oc cp $(oc get pods -l 'deploymentConfig=backend-redis' -o json | jq '.items[0].metadata.name' -r):/var/lib/redis/data/dump.rdb ${BACKUP_DIR}/backend-redis-dump.rdb
+
+echo "Backup system-redis"
+
+oc cp $(oc get pods -l 'deploymentConfig=system-redis' -o json | jq '.items[0].metadata.name' -r):/var/lib/redis/data/dump.rdb ${BACKUP_DIR}/system-redis-dump.rdb
+
+
