4a9468c33e489032ad460b6cb2abf4aebe0ac7cd
Steven Ellis <steven.ellis@gmail.com>
2019-09-11 13:48:49 +0800

    Add support to scale individual AZs

--- scale_workers.sh
+++ scale_workers.sh
@@ -40,8 +40,22 @@ scale_up ()
     watch "echo 'wait for our new machines to be READY'; oc get machinesets -n openshift-machine-api"
 }
 
+# scale_up_az
+#
+# $1 = a|b|c = Amazon Availability Zone
+#
+scale_up_az ()
+{
+    WORKING_CLUSTER=${CLUSTERID}-worker-${OCP_REGION}${1}
+    oc scale --replicas=2 machineset ${WORKING_CLUSTER} -n ${OCP_NAMESPACE}
+
+    # We now need to wait for them to all be created
+    watch "echo 'wait for our new machines to be READY in ${OCP_REGION}${1}'; oc get machinesets -n openshift-machine-api"
+}
+
 scale_down ()
 {
+    WORKING_CLUSTER=${CLUSTERID}-worker-${OCP_REGION}${1}
     oc scale --replicas=1 machineset ${CLUSTER_A} -n ${OCP_NAMESPACE}
     oc scale --replicas=1 machineset ${CLUSTER_B} -n ${OCP_NAMESPACE}
     oc scale --replicas=1 machineset ${CLUSTER_C} -n ${OCP_NAMESPACE}
@@ -62,10 +76,25 @@ case "$1" in
         pre_setup
         scale_down
         ;;
+  aza)
+        pre_setup
+        scale_up_az a
+        ;;
+  azb)
+        pre_setup
+        scale_up_az b
+        ;;
+  azc)
+        pre_setup
+        scale_up_az c
+        ;;
   *)
-        echo "Usage: $N {up|down}" >&2
+        echo "Usage: $N {up|down|aza|azb|azc}" >&2
         echo " up - scale to 2 replicas for all worker nodes" >&2
-        echo " up - scale to 1 replica for all worker nodes" >&2
+        echo " down - scale to 1 replica for all worker nodes" >&2
+        echo " aza - scale to 2 replicas for worker nodes in AZ ${OCP_REGION}a" >&2
+        echo " azb - scale to 2 replicas for worker nodes in AZ ${OCP_REGION}b" >&2
+        echo " azc - scale to 2 replicas for worker nodes in AZ ${OCP_REGION}c" >&2
         exit 1
         ;;
 esac
