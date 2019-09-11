57da2cb6016fca48aaaf17c2f8cbe9e2de138d2a
jumperwire <hendrasutanto@gmail.com>
2019-09-11 12:55:52 +0800

    Create quarkus.sh

---
+++ quarkus.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+#
+# Deploy or cleanup a quarkus install
+
+# Step 0 - Our master environment
+source ./ocp.env
+source ./functions
+
+# And login as the kubeadmin user
+
+oc login -u ${OCP_USER} -p ${OCP_PASS} ${OCP_ENDPOINT} --insecure-skip-tls-verify=false
+
+OCP_NAMESPACE=supersonic-subatomic-java
+
+
+setup_quarkus()
+{
+    echo "Create the supersonic-subatomic-java namespace"
+    oc new-project supersonic-subatomic-java
+
+    echo "Deploy big fat java"
+    oc apply -f https://raw.githubusercontent.com/jumperwire/supersonic-subatomic-java/master/deploy-big-fat-java.yaml
+    
+    echo "Deploy supersonic-subatomic java"
+    oc apply -f https://raw.githubusercontent.com/jumperwire/supersonic-subatomic-java/master/deploy-supersonic-subatomic-java.yaml
+    
+    echo "Create big fat java service"
+    oc apply -f https://raw.githubusercontent.com/jumperwire/supersonic-subatomic-java/master/service-big-fat-java.yaml
+    
+    echo "Create supersonic-subatomic java service"
+    oc apply -f https://raw.githubusercontent.com/jumperwire/supersonic-subatomic-java/master/service-supersonic-subatomic-java.yaml
+    
+    echo "Create big fat java route"
+    oc expose service big-fat-java
+    
+    echo "Create supersonic-subatomic java route"
+    oc expose service supersonic-subatomic-java
+}
+
+cleanup_quarkus()
+{
+    echo "Clean up our Quarkus environments and remove the Operator"
+    echo "Scaling big fat java back to 1 pod"
+    oc scale --replicas=1 deployment.apps big-fat-java
+    
+    echo "Scaling big fat java back to 1 pod"
+    oc scale --replicas=1 deployment.apps supersonic-subatomic-java
+}
+
+
+case "$1" in
+  setup)
+        setup_quarkus
+        ;;
+  delete|cleanup|remove)
+        cleanup_quarkus
+        ;;
+  *)
+        echo "Usage: $N {setup|delete}" >&2
+        exit 1
+        ;;
+esac
