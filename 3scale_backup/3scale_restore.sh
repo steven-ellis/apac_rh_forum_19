0f550267c47c11c4c21fb6ce50c6b5cf2417decf
Steven Ellis <steven.ellis@gmail.com>
2019-09-16 12:09:58 +0900

    Add environment re-write capability to 3scale restore

--- 3scale_backup/3scale_restore.sh
+++ 3scale_backup/3scale_restore.sh
@@ -5,66 +5,107 @@
 # and
 #  - https://access.redhat.com/documentation/en-us/red_hat_3scale_api_management/2.6/html/operating_3scale/backup-restore#backup_procedures
 #
+# [1] = backup directory
+# [2] = OCP namespace/project for 3scale
+
 
 BACKUP_DIR=${1:-backup}
 
 # Source our environment
 source ../ocp.env
 source ../functions
+source ../3scale.env
+
+OCP_NAMESPACE=${2:-$API_MANAGER_NS}
 
 # And login as the kubeadmin user
 
 oc login -u ${OCP_USER} -p ${OCP_PASS} ${OCP_ENDPOINT} --insecure-skip-tls-verify=false
 
-echo "Restoring a new 3scale backup from directory ${BACKUP_DIR}"
+echo "Restoring a new 3scale backup from directory ${BACKUP_DIR} into project ${OCP_NAMESPACE}"
 
 if [ ! -d ${BACKUP_DIR} ]; then
     echo "Missing Backup Directore ${BACKUP_DIR}"
     exit -1
 fi
 
-echo "Restore mySQL"
-echo "  Copy the MySQL dump to the system-mysql pod:"
-oc rsh $(oc get pods -l 'deploymentConfig=system-mysql' -o json | jq -r '.items[0].metadata.name') bash -c 'export MYSQL_PWD=${MYSQL_ROOT_PASSWORD}; mysqldump --single-transaction -hsystem-mysql -uroot system' | gzip > ${BACKUP_DIR}/system-mysql-backup.gz
+if [ ! -f ${BACKUP_DIR}/backup.env ]; then
+    echo "Invalid backup in ${BACKUP_DIR} - missing backup.env"
+    exit -1
+fi
 
-oc cp ${BACKUP_DIR}/system-mysql-backup.gz $(oc get pods -l 'deploymentConfig=system-mysql' -o json | jq '.items[0].metadata.name' -r):/var/lib/mysql
+source ${BACKUP_DIR}/backup.env
+
+# rewrite_db_backup 
+#
+# arg1 - src file
+# arg2 - dest file
+#
+rewrite_db_backup ()
+{
+   
+    if [ ! -f ${1} ]; then
+        echo "Missing database file ${1} for rewrite"
+        exit -1
+    fi
+
+    echo "  Re-write the cluster information for database ${1}"
+    echo "  into file ${2}"
+    gunzip -c ${1} | \
+    sed "s/${OLD_NAMESPACE}/${OCP_NAMESPACE}/g;s/${OLD_DOMAIN}=/${OCP_DOMAIN}/g" | \
+    gzip -c - > ${2}
+}
+
+
+MYSQL_POD=$(oc get pods -l 'deploymentConfig=system-mysql' -o json | jq -r '.items[0].metadata.name')
+echo "Restore mySQL to ${MYSQL_POD}"
+
+echo "  Re-write the cluster information:"
+rewrite_db_backup ${BACKUP_DIR}/system-mysql-backup.gz ${BACKUP_DIR}/system-mysql-restore.gz
+
+echo "  Copy the MySQL dump to the system-mysql pod:"
+
+##oc cp ${BACKUP_DIR}/system-mysql-restore.gz ${MYSQL_POD}:/var/lib/mysql
 
 echo "  Decompress the Backup File:"
 
-oc rsh $(oc get pods -l 'deploymentConfig=system-mysql' -o json | jq -r '.items[0].metadata.name') bash -c 'gzip -d ${HOME}/system-mysql-backup.gz'
+##oc rsh ${MYSQL_POD} bash -c 'gzip -d ${HOME}/system-mysql-restore.gz'
 
 echo "  Restore the MySQL DB Backup file:"
 
-oc rsh $(oc get pods -l 'deploymentConfig=system-mysql' -o json | jq -r '.items[0].metadata.name') bash -c 'export MYSQL_PWD=${MYSQL_ROOT_PASSWORD}; mysql -hsystem-mysql -uroot system < ${HOME}/system-mysql-backup'
+##oc rsh ${MYSQL_POD} bash -c 'export MYSQL_PWD=${MYSQL_ROOT_PASSWORD}; mysql -hsystem-mysql -uroot system < ${HOME}/system-mysql-restore'
 
 
 
 echo "Restore System App"
 
-
-oc rsync ${BACKUP_DIR}/system/ $(oc get pods -l 'deploymentConfig=system-app' -o json | jq '.items[0].metadata.name' -r):/opt/system/public/system
+##oc rsync ${BACKUP_DIR}/system/ $(oc get pods -l 'deploymentConfig=system-app' -o json | jq '.items[0].metadata.name' -r):/opt/system/public/system
 
 
 echo "Restore zync database"
+PSQL_POD=$(oc get pods -l 'deploymentConfig=zync-database' -o json | jq -r '.items[0].metadata.name')
 
-echo "  Copy the Zync Database dump to the zync-database pod:"
+echo "  Re-write the cluster information:"
+rewrite_db_backup ${BACKUP_DIR}/zync-database-backup.gz ${BACKUP_DIR}/zync-database-restore.gz
 
-oc cp ${BACKUP_DIR}/zync-database-backup.gz $(oc get pods -l 'deploymentConfig=zync-database' -o json | jq '.items[0].metadata.name' -r):/var/lib/pgsql/
+echo "  Copy the Zync Database dump to the zync-database pod ${PSQL_POD}:"
+
+##oc cp ${BACKUP_DIR}/zync-database-restore.gz ${PSQL_POD}:/var/lib/pgsql/
 
 echo "  Decompress the Backup File:"
 
-oc rsh $(oc get pods -l 'deploymentConfig=zync-database' -o json | jq -r '.items[0].metadata.name') bash -c 'gzip -d ${HOME}/zync-database-backup.gz'
+##oc rsh ${PSQL_POD} bash -c 'gzip -d ${HOME}/zync-database-restore.gz'
 
 echo "  Restore the PostgreSQL DB Backup file:"
 
-oc rsh $(oc get pods -l 'deploymentConfig=zync-database' -o json | jq -r '.items[0].metadata.name') bash -c 'psql -f ${HOME}/zync-database-backup'
+##oc rsh ${PSQL_POD} bash -c 'psql -f ${HOME}/zync-database-backup'
 
 
 #echo "Restore backend-redis"
 
-echo "Currently refer to documentation"
-echo "https://access.redhat.com/documentation/en-us/red_hat_3scale_api_management/2.6/html/operating_3scale/backup-restore#backup_procedures"
-exit 1
+#echo "Currently refer to documentation"
+#echo "https://access.redhat.com/documentation/en-us/red_hat_3scale_api_management/2.6/html/operating_3scale/backup-restore#backup_procedures"
+#exit 1
 
 
 # Basically We need a way to patch the config map
@@ -147,6 +188,6 @@ restore_redis ()
     oc get pods -l "deploymentConfig=${1}-redis" -o json | jq '.items[0].metadata.name' -r
 }
 
-restore_redis backend
-restore_redis system
+#restore_redis backend
+#restore_redis system
 
