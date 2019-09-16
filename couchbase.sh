d552eabee1552b36a56cdb3e2a1425ede16e4797
Steven Ellis <steven.ellis@gmail.com>
2019-09-16 13:15:46 +0900

    move couchbase to printInfo

--- couchbase.sh
+++ couchbase.sh
@@ -12,39 +12,39 @@ OCP_NAMESPACE=couchbase
 
 setup_couchbase()
 {
-    echo "Create the Couchbase namespace"
+    printInfo "Create the Couchbase namespace"
     oc new-project couchbase
 
-    echo "Populate our secret"
+    printInfo "Populate our secret"
     oc create -f cbauthsecret.yaml -n ${OCP_NAMESPACE}
 }
 
 cleanup_couchbase()
 {
-    echo "Clean up our Couchbase environments and remove the Operator"
-    echo "Don't worry if your see - No resources found"
-    echo "Remove couchbase subscription"
+    printInfo "Clean up our Couchbase environments and remove the Operator"
+    printInfo "Don't worry if your see - No resources found"
+    printInfo "Remove couchbase subscription"
     #oc delete subscriptions couchbase-enterprise-certified -n ${OCP_NAMESPACE}
     oc delete subscriptions -l csc-owner-name=installed-certified-couchbase -n ${OCP_NAMESPACE}
     oc delete clusterserviceversion couchbase-operator.v1.1.0 -n ${OCP_NAMESPACE}
 
-    echo "Make sure we've remove couchbase from our cataloge sources"
+    printInfo "Make sure we've remove couchbase from our cataloge sources"
     oc delete catalogsourceconfig -n openshift-marketplace installed-certified-couchbase
 
 
-    echo "Remove couchbase operator"
+    printInfo "Remove couchbase operator"
     oc delete deployment -l app=couchbase-operator -n ${OCP_NAMESPACE}
     oc delete replicaset -l app=couchbase-operator -n ${OCP_NAMESPACE}
     oc delete all -l app=couchbase-operator -n ${OCP_NAMESPACE}
 
-    echo "Remove all couchbase instances"
+    printInfo "Remove all couchbase instances"
     oc delete all -l app=couchbase -n ${OCP_NAMESPACE}
 
 
-    echo "Remove our secret"
+    printInfo "Remove our secret"
     oc delete -f cbauthsecret.yaml -n ${OCP_NAMESPACE}
 
-    echo "Remove the Couchbase namespace"
+    printInfo "Remove the Couchbase namespace"
     oc delete namespace ${OCP_NAMESPACE}
 
 }
