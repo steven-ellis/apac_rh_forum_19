a206325614b7ad9d9a1071725b122d9f5515c101
Steven Ellis <steven.ellis@gmail.com>
2019-09-19 17:06:24 +0900

    Tidy up code alignment in 3scale restore script

--- 3scale_backup/3scale_restore.sh
+++ 3scale_backup/3scale_restore.sh
@@ -5,22 +5,25 @@
 # and
 #  - https://access.redhat.com/documentation/en-us/red_hat_3scale_api_management/2.6/html/operating_3scale/backup-restore#backup_procedures
 #
-# [1] = backup directory
-# [2] = OCP namespace/project for 3scale
-
-
-BACKUP_DIR=${1:-backup}
+# [1] = backup directory - REQUIRED
+# [2] = override OCP namespace/project for 3scale
 
 # Source our environment
 source ../ocp.env
 source ../functions
 source ../3scale.env
 
+
+if [ "${1}missing" == "missing" ]; then
+    echo "Usage: $1 <backup_dir> [optional 3scale namespace]" >&2
+    exit 1
+fi
+
+BACKUP_DIR=${1}
 OCP_NAMESPACE=${2:-$API_MANAGER_NS}
 
 # And login as the kubeadmin user
-
-oc login -u ${OCP_USER} -p ${OCP_PASS} ${OCP_ENDPOINT} --insecure-skip-tls-verify=false
+oc_login
 
 if ( ! projectExists ${OCP_NAMESPACE}); then
     printERROR "No 3scale service deployed under ${OCP_NAMESPACE} - ${1} Exiting"
@@ -71,83 +74,75 @@ rewrite_db_backup ()
 
 restore_system_mysql ()
 {
-MYSQL_POD=$(oc get pods -l 'deploymentConfig=system-mysql' -o json | jq -r '.items[0].metadata.name')
-printInfo "Restore mySQL to ${MYSQL_POD}"
+    MYSQL_POD=$(oc get pods -l 'deploymentConfig=system-mysql' -o json | jq -r '.items[0].metadata.name')
+    printInfo "Restore mySQL to ${MYSQL_POD}"
 
-printInfo "  Re-write the cluster information:"
-rewrite_db_backup ${BACKUP_DIR}/system-mysql-backup.gz ${BACKUP_DIR}/system-mysql-restore.gz
+    printInfo "  Re-write the cluster information:"
+    rewrite_db_backup ${BACKUP_DIR}/system-mysql-backup.gz ${BACKUP_DIR}/system-mysql-restore.gz
 
-printInfo "  Make sure we've got no old backups on the pod"
-oc rsh ${MYSQL_POD} bash -c 'rm -f /var/lib/mysql/system-mysql-restore'
+    printInfo "  Make sure we've got no old backups on the pod"
+    oc rsh ${MYSQL_POD} bash -c 'rm -f /var/lib/mysql/system-mysql-restore'
 
-printInfo "  Copy the MySQL dump to the system-mysql pod:"
-oc cp ${BACKUP_DIR}/system-mysql-restore.gz ${MYSQL_POD}:/var/lib/mysql
+    printInfo "  Copy the MySQL dump to the system-mysql pod:"
+    oc cp ${BACKUP_DIR}/system-mysql-restore.gz ${MYSQL_POD}:/var/lib/mysql
 
-printInfo "  Decompress the Backup File:"
+    printInfo "  Decompress the Backup File:"
+    oc rsh ${MYSQL_POD} bash -c 'gzip -d ${HOME}/system-mysql-restore.gz'
 
-oc rsh ${MYSQL_POD} bash -c 'gzip -d ${HOME}/system-mysql-restore.gz'
+    printInfo "  Restore the MySQL DB Backup file:"
+    oc rsh ${MYSQL_POD} bash -c 'export MYSQL_PWD=${MYSQL_ROOT_PASSWORD}; mysql -hsystem-mysql -uroot system < ${HOME}/system-mysql-restore'
 
-printInfo "  Restore the MySQL DB Backup file:"
-
-oc rsh ${MYSQL_POD} bash -c 'export MYSQL_PWD=${MYSQL_ROOT_PASSWORD}; mysql -hsystem-mysql -uroot system < ${HOME}/system-mysql-restore'
-printInfo "mySQL restore to ${MYSQL_POD} completed"
+    printInfo "mySQL restore to ${MYSQL_POD} completed"
 }
 
 
 
 restore_system_app ()
 {
-printInfo "Restore System App"
-printWarning "Currently we might get an error that the destination directory is missing"
+    printInfo "Restore System App"
+    printWarning "Currently we might get an error that the destination directory is missing"
 
-oc rsync ${BACKUP_DIR}/system/ $(oc get pods -l 'deploymentConfig=system-app' -o json | jq '.items[0].metadata.name' -r):/opt/system/public/system
+    oc rsync ${BACKUP_DIR}/system/ $(oc get pods -l 'deploymentConfig=system-app' -o json | jq '.items[0].metadata.name' -r):/opt/system/public/system
 }
 
 
 restore_zync_database ()
 {
-printInfo "Restore zync database"
+    printInfo "Restore zync database"
 PSQL_POD=$(oc get pods -l 'deploymentConfig=zync-database' -o json | jq -r '.items[0].metadata.name')
 
-printInfo "Scale down zync and zyncq-que ahead of restore"
-oc scale --replicas=0  dc zync
-oc scale --replicas=0  dc zync-que
+    printInfo "Scale down zync and zyncq-que ahead of restore"
+    oc scale --replicas=0  dc zync
+    oc scale --replicas=0  dc zync-que
 
-printInfo "  Re-write the cluster information:"
-rewrite_db_backup ${BACKUP_DIR}/zync-database-backup.gz ${BACKUP_DIR}/zync-database-restore.gz
+    printInfo "  Re-write the cluster information:"
+    rewrite_db_backup ${BACKUP_DIR}/zync-database-backup.gz ${BACKUP_DIR}/zync-database-restore.gz
 
-printInfo "  Make sure we've got no old backups on the pod"
-oc rsh ${PSQL_POD} bash -c 'rm -f ${HOME}/zync-database-restore*'
+    printInfo "  Make sure we've got no old backups on the pod"
+    oc rsh ${PSQL_POD} bash -c 'rm -f ${HOME}/zync-database-restore*'
 
-printInfo "  Copy the Zync Database dump to the zync-database pod ${PSQL_POD}:"
-oc cp ${BACKUP_DIR}/zync-database-restore.gz ${PSQL_POD}:/var/lib/pgsql/
+    printInfo "  Copy the Zync Database dump to the zync-database pod ${PSQL_POD}:"
+    oc cp ${BACKUP_DIR}/zync-database-restore.gz ${PSQL_POD}:/var/lib/pgsql/
 
-printInfo "  Decompress the Backup File:"
-oc rsh ${PSQL_POD} bash -c 'gzip -d ${HOME}/zync-database-restore.gz'
+    printInfo "  Decompress the Backup File:"
+    oc rsh ${PSQL_POD} bash -c 'gzip -d ${HOME}/zync-database-restore.gz'
 
-printInfo "  Check we've scaled down ahead of restore"
-oc get dc -l threescale_component=zync
+    printInfo "  Check we've scaled down ahead of restore"
+    oc get dc -l threescale_component=zync
 
-printInfo "  Restore the PostgreSQL DB Backup file:"
-oc rsh ${PSQL_POD} bash -c 'psql -f ${HOME}/zync-database-restore'
+    printInfo "  Restore the PostgreSQL DB Backup file:"
+    oc rsh ${PSQL_POD} bash -c 'psql -f ${HOME}/zync-database-restore'
 
-printInfo "Scale up zync and zyncq-que now the database is restored"
-oc scale --replicas=1  dc zync
-oc scale --replicas=1  dc zync-que
+    printInfo "Scale up zync and zyncq-que now the database is restored"
+    oc scale --replicas=1  dc zync
+    oc scale --replicas=1  dc zync-que
 
-printInfo "  Confirm we've scaled the zync pods back up"
-sleep 2s
-oc get dc -l threescale_component=zync
+    printInfo "  Confirm we've scaled the zync pods back up"
+    sleep 2s
+    oc get dc -l threescale_component=zync
 
-printInfo "zync database restore to ${PSQL_POD} completed"
+    printInfo "zync database restore to ${PSQL_POD} completed"
 }
-
-
-
-#printInfo "Currently refer to documentation"
-#echo "https://access.redhat.com/documentation/en-us/red_hat_3scale_api_management/2.6/html/operating_3scale/backup-restore#backup_procedures"
-#exit 1
-
 
 # Basically We need a way to patch the config map
 
