63d4523a92006c039914058e1d1c206b94c9d71f
Steven Ellis <steven.ellis@gmail.com>
2019-09-19 12:26:15 +0900

    make 3scale restore scale down the zync pods

--- 3scale_backup/3scale_restore.sh
+++ 3scale_backup/3scale_restore.sh
@@ -27,90 +27,122 @@ if ( ! projectExists ${OCP_NAMESPACE}); then
     exit -1
 fi  
 
-oc project ${OCP_NAMESPACE}
-
-echo "Restoring a new 3scale backup from directory ${BACKUP_DIR} into project ${OCP_NAMESPACE}"
-
 if [ ! -d ${BACKUP_DIR} ]; then
-    echo "Missing Backup Directore ${BACKUP_DIR}"
+    printError "Missing Backup Directore ${BACKUP_DIR}"
     exit -1
 fi
 
 if [ ! -f ${BACKUP_DIR}/backup.env ]; then
-    echo "Invalid backup in ${BACKUP_DIR} - missing backup.env"
+    printError "Invalid backup in ${BACKUP_DIR} - missing backup.env"
     exit -1
 fi
 
 source ${BACKUP_DIR}/backup.env
+
+oc project ${OCP_NAMESPACE}
+
+printInfo "Restoring a new 3scale backup from directory ${BACKUP_DIR} into project ${OCP_NAMESPACE}"
 
 # rewrite_db_backup 
 #
 # arg1 - src file
 # arg2 - dest file
 #
+# We need the second sed line due to wierd data in the mysql backup
+#
 rewrite_db_backup ()
 {
    
     if [ ! -f ${1} ]; then
-        echo "Missing database file ${1} for rewrite"
+        printError "Missing database file ${1} for rewrite"
         exit -1
     fi
 
-    echo "  Re-write the cluster information for database ${1}"
-    echo "  into file ${2}"
+    printInfo "  Re-write the cluster information for database ${1}"
+    printInfo "  into file ${2}"
     gunzip -c ${1} | \
     sed "s/${OLD_NAMESPACE}/${OCP_NAMESPACE}/g;s/${OLD_DOMAIN}=/${OCP_DOMAIN}/g" | \
+    sed "s/\^\[\[Z-/-/g" | \
     gzip -c - > ${2}
 }
 
 
+restore_system_mysql ()
+{
 MYSQL_POD=$(oc get pods -l 'deploymentConfig=system-mysql' -o json | jq -r '.items[0].metadata.name')
-echo "Restore mySQL to ${MYSQL_POD}"
+printInfo "Restore mySQL to ${MYSQL_POD}"
 
-echo "  Re-write the cluster information:"
+printInfo "  Re-write the cluster information:"
 rewrite_db_backup ${BACKUP_DIR}/system-mysql-backup.gz ${BACKUP_DIR}/system-mysql-restore.gz
 
-echo "  Copy the MySQL dump to the system-mysql pod:"
+printInfo "  Make sure we've got no old backups on the pod"
+oc rsh ${MYSQL_POD} bash -c 'rm -f /var/lib/mysql/system-mysql-restore'
 
-##oc cp ${BACKUP_DIR}/system-mysql-restore.gz ${MYSQL_POD}:/var/lib/mysql
+printInfo "  Copy the MySQL dump to the system-mysql pod:"
+oc cp ${BACKUP_DIR}/system-mysql-restore.gz ${MYSQL_POD}:/var/lib/mysql
 
-echo "  Decompress the Backup File:"
+printInfo "  Decompress the Backup File:"
 
-##oc rsh ${MYSQL_POD} bash -c 'gzip -d ${HOME}/system-mysql-restore.gz'
+oc rsh ${MYSQL_POD} bash -c 'gzip -d ${HOME}/system-mysql-restore.gz'
 
-echo "  Restore the MySQL DB Backup file:"
+printInfo "  Restore the MySQL DB Backup file:"
 
-##oc rsh ${MYSQL_POD} bash -c 'export MYSQL_PWD=${MYSQL_ROOT_PASSWORD}; mysql -hsystem-mysql -uroot system < ${HOME}/system-mysql-restore'
-
-
-
-echo "Restore System App"
-
-##oc rsync ${BACKUP_DIR}/system/ $(oc get pods -l 'deploymentConfig=system-app' -o json | jq '.items[0].metadata.name' -r):/opt/system/public/system
+oc rsh ${MYSQL_POD} bash -c 'export MYSQL_PWD=${MYSQL_ROOT_PASSWORD}; mysql -hsystem-mysql -uroot system < ${HOME}/system-mysql-restore'
+printInfo "mySQL restore to ${MYSQL_POD} completed"
+}
 
 
-echo "Restore zync database"
+
+restore_system_app ()
+{
+printInfo "Restore System App"
+printWarning "Currently we might get an error that the destination directory is missing"
+
+oc rsync ${BACKUP_DIR}/system/ $(oc get pods -l 'deploymentConfig=system-app' -o json | jq '.items[0].metadata.name' -r):/opt/system/public/system
+}
+
+
+restore_zync_database ()
+{
+printInfo "Restore zync database"
 PSQL_POD=$(oc get pods -l 'deploymentConfig=zync-database' -o json | jq -r '.items[0].metadata.name')
 
-echo "  Re-write the cluster information:"
+printInfo "Scale down zync and zyncq-que ahead of restore"
+oc scale --replicas=0  dc zync
+oc scale --replicas=0  dc zync-que
+
+printInfo "  Re-write the cluster information:"
 rewrite_db_backup ${BACKUP_DIR}/zync-database-backup.gz ${BACKUP_DIR}/zync-database-restore.gz
 
-echo "  Copy the Zync Database dump to the zync-database pod ${PSQL_POD}:"
+printInfo "  Make sure we've got no old backups on the pod"
+oc rsh ${PSQL_POD} bash -c 'rm -f ${HOME}/zync-database-restore*'
 
-##oc cp ${BACKUP_DIR}/zync-database-restore.gz ${PSQL_POD}:/var/lib/pgsql/
+printInfo "  Copy the Zync Database dump to the zync-database pod ${PSQL_POD}:"
+oc cp ${BACKUP_DIR}/zync-database-restore.gz ${PSQL_POD}:/var/lib/pgsql/
 
-echo "  Decompress the Backup File:"
+printInfo "  Decompress the Backup File:"
+oc rsh ${PSQL_POD} bash -c 'gzip -d ${HOME}/zync-database-restore.gz'
 
-##oc rsh ${PSQL_POD} bash -c 'gzip -d ${HOME}/zync-database-restore.gz'
+printInfo "  Check we've scaled down ahead of restore"
+oc get dc -l threescale_component=zync
 
-echo "  Restore the PostgreSQL DB Backup file:"
+printInfo "  Restore the PostgreSQL DB Backup file:"
+oc rsh ${PSQL_POD} bash -c 'psql -f ${HOME}/zync-database-restore'
 
-##oc rsh ${PSQL_POD} bash -c 'psql -f ${HOME}/zync-database-backup'
+printInfo "Scale up zync and zyncq-que now the database is restored"
+oc scale --replicas=1  dc zync
+oc scale --replicas=1  dc zync-que
+
+printInfo "  Confirm we've scaled the zync pods back up"
+sleep 2s
+oc get dc -l threescale_component=zync
+
+printInfo "zync database restore to ${PSQL_POD} completed"
+}
 
 
-#echo "Restore backend-redis"
 
-#echo "Currently refer to documentation"
+#printInfo "Currently refer to documentation"
 #echo "https://access.redhat.com/documentation/en-us/red_hat_3scale_api_management/2.6/html/operating_3scale/backup-restore#backup_procedures"
 #exit 1
 
@@ -125,76 +157,81 @@ restore_redis ()
 {
 
     if [ "${1}" != "system" ] && [ "${1}" != "backend" ]; then
-	echo "restore_redis only supports the arguments" >&2
-	echo "system | backend" >&2
+	printError "restore_redis only supports the arguments" >&2
+	printError "system | backend" >&2
 	exit 1
     fi
 
-    echo "Restore ${1}-redis"
-    echo "Current ${1}-redis pods"
+    printInfo "Restore ${1}-redis"
+    printInfo "Current ${1}-redis pods"
     oc get pods -l "deploymentConfig=${1}-redis" -o json | jq '.items[0].metadata.name' -r
-    echo "Modify the redis-config configmap:"
+    printInfo "Modify the redis-config configmap:"
 
     oc get --export configmap redis-config -o yaml > redis-config.orig.yaml
     cat redis-config.orig.yaml | sed "s/ save / #save /g;s/ appendonly yes/ appendonly no/" > redis-config.new.yaml
     oc apply -f redis-config.new.yaml
 
-    echo "Redeploy ${1}-redis to load the new configurations:"
+    printInfo "Redeploy ${1}-redis to load the new configurations:"
 
     oc rollout latest dc/${1}-redis
 
-    echo "Sleep 20s while the rollout actions"
+    printInfo "Sleep 20s while the rollout actions"
     sleep 20s
     oc_wait_for  pod 3scale-api-management app ${API_MANAGER_NS}
     oc_wait_for  pod ${1}-redis deploymentconfig ${API_MANAGER_NS}
 
-    echo "Current ${1}-redis pods"
+    printInfo "Current ${1}-redis pods"
     oc get pods -l "deploymentConfig=${1}-redis" -o json | jq '.items[0].metadata.name' -r
-    echo "Sleep 10s while the container stabilises"
+    printInfo "Sleep 10s while the container stabilises"
     sleep 10s
-    echo "Current ${1}-redis pods"
+    printInfo "Current ${1}-redis pods"
     oc get pods -l "deploymentConfig=${1}-redis" -o json | jq '.items[0].metadata.name' -r
 
-    echo "Rename the dump.rb file:"
+    printInfo "Rename the dump.rb file:"
 
     oc rsh $(oc get pods -l "deploymentConfig=${1}-redis" -o json | jq '.items[0].metadata.name' -r) bash -c 'mv ${HOME}/data/dump.rdb ${HOME}/data/dump.rdb-old'
 
-    echo "Rename the appendonly.aof file:"
+    printInfo "Rename the appendonly.aof file:"
 
     oc rsh $(oc get pods -l "deploymentConfig=${1}-redis" -o json | jq '.items[0].metadata.name' -r) bash -c 'mv ${HOME}/data/appendonly.aof ${HOME}/data/appendonly.aof-old'
 
-    echo "Move the Backup file to the POD:"
+    printInfo "Move the Backup file to the POD:"
 
     oc cp ./${1}-redis-dump.rdb $(oc get pods -l "deploymentConfig=${1}-redis" -o json | jq '.items[0].metadata.name' -r):/var/lib/redis/data/dump.rdb
 
-    echo "Redeploy ${1}-redis to load the backup:"
+    printInfo "Redeploy ${1}-redis to load the backup:"
 
     oc rollout latest dc/${1}-redis
 
-    echo "Sleep 20s while the rollout actions"
+    printInfo "Sleep 20s while the rollout actions"
     sleep 20s
     oc_wait_for  pod 3scale-api-management app ${API_MANAGER_NS}
     oc_wait_for  pod ${1}-redis deploymentconfig ${API_MANAGER_NS}
 
-    echo "Modify the redis-config configmap back to its original settings:"
+    printInfo "Modify the redis-config configmap back to its original settings:"
     oc apply -f redis-config.orig.yaml
 
-    echo "Redeploy ${1}-redis to reload the default configurations:"
+    printInfo "Redeploy ${1}-redis to reload the default configurations:"
 
     oc rollout latest dc/${1}-redis
-    echo "Sleep 20s while the rollout actions"
+    printInfo "Sleep 20s while the rollout actions"
     sleep 20s
     oc_wait_for  pod 3scale-api-management app ${API_MANAGER_NS}
     oc_wait_for  pod ${1}-redis deploymentconfig ${API_MANAGER_NS}
 
-    echo "Current ${1}-redis pods"
+    printInfo "Current ${1}-redis pods"
     oc get pods -l "deploymentConfig=${1}-redis" -o json | jq '.items[0].metadata.name' -r
-    echo "Sleep 10s while the container stabilises"
+    printInfo "Sleep 10s while the container stabilises"
     sleep 10s
-    echo "Current ${1}-redis pods"
+    printInfo "Current ${1}-redis pods"
     oc get pods -l "deploymentConfig=${1}-redis" -o json | jq '.items[0].metadata.name' -r
 }
 
+restore_system_mysql
+restore_zync_database
+restore_system_app
+
+printInfo "Restore backend-redis"
 #restore_redis backend
 #restore_redis system
 
