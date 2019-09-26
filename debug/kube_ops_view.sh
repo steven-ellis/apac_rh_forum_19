bda8837e2a78ca11e9da8c5fc3ca6fef5cdd108c
Steven Ellis <steven.ellis@gmail.com>
2019-09-26 15:00:26 +1200

    Add deployment for kube ops debug tool

---
+++ debug/kube_ops_view.sh
@@ -0,0 +1,75 @@
+
+#!/bin/bash
+#
+# This leverages
+#  - https://blog.openshift.com/full-cluster-capacity-management-monitoring-openshift/
+#  - https://github.com/raffaelespazzoli/kube-ops-view
+#  - https://github.com/hjacobs/kube-ops-view
+# Documented under
+#  - https://kubernetes-operational-view.readthedocs.io/
+#
+
+source ./ocp.env
+source ./functions
+
+OCP_NAMESPACE=ocp-ops-view
+
+deploy_kubeops ()
+{
+
+    oc new-project ${OCP_NAMESPACE}
+    oc adm policy add-scc-to-user anyuid -z default -n ocp-ops-view
+    oc apply -f https://raw.githubusercontent.com/raffaelespazzoli/kube-ops-view/master/ocp-ops-view.yaml
+
+    oc expose svc kube-ops-view-stable-kube-ops-view
+}
+
+cleanup_kubeops ()
+{
+    oc delete -n ${OCP_NAMESPACE} \
+       -f //raw.githubusercontent.com/raffaelespazzoli/kube-ops-view/master/ocp-ops-view.yaml
+    
+    oc delete svc kube-ops-view-stable-kube-ops-view
+
+    echo "Deleting the project ${OCP_NAMESPACE}"
+    echo "This might take a couple of minutes to return"
+
+    oc delete project ${OCP_NAMESPACE}
+}
+
+status_kubeops ()
+{
+    oc get route -n ${OCP_NAMESPACE} | grep kube-ops-view | awk '{print $2}'
+    #oc get route -n ${OCP_NAMESPACE} --template "{{.spec.host}}"
+}
+
+case "$1" in
+  setup)
+        oc_login
+        if (projectExists ${OCP_NAMESPACE}); then
+	    printWarning "Service Kube Ops View already deployed in ${OCP_NAMESPACE} - Exiting"
+        else
+            deploy_kubeops
+            status_kubeops
+        fi
+        ;;
+  status)
+        oc_login
+        if (projectExists ${OCP_NAMESPACE}); then
+            status_kubeops
+        fi
+        ;;
+  delete|cleanup|remove)
+        oc_login
+        if (projectExists ${OCP_NAMESPACE}); then
+            cleanup_kubeops
+        fi
+        ;;
+  *)
+	echo "Usage: $1 {setup|status|delete|cleanup|remove}" >&2
+        exit 1
+        ;;
+esac
+
+
+
